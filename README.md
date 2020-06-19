f2c5bbfe58edf28f85191d16d8df63cf76304888
this-abhi98 <59477311+this-abhi98@users.noreply.github.com>
2020-06-19 17:58:56 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# jsx
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const img = "https://picsum.photos/200/300";
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+
+    <div>
+      <img src={img} />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
